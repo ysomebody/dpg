ab180211c21f9d0241285569a454f679144694cb
dennis.ying <dennis.ying@ni.com>
2020-01-06 16:07:20 +0800

    Add function to generate pattern file as a placeholder.

--- dpg/digital_pattern_generator.py
+++ dpg/digital_pattern_generator.py
@@ -2,16 +2,18 @@ import csv
 from dpg.spi_4w.spi_4w import DigitalPatternGenerator_SPI4W
 
 class DigitalPatternGenerator:
-    def __init__(self, config_file, pattern_file, timeset_between_frames):
+    def __init__(self, config_file, timeset_between_frames):
         self.config_file = config_file
         self.timeset_between_frames = timeset_between_frames
 
-    def generate_digital_pattern_src(self):
+    def generate_digital_pattern_string(self):
         with open(self.config_file) as csvfile:
             configs = csv.DictReader(csvfile, delimiter=',')
             vectors = DigitalPatternGenerator_SPI4W(self.timeset_between_frames).generate(configs)
-            src = '\n'.join(v.get_statement() for v in vectors)
-            print(src)
+            return '\n'.join(v.get_statement() for v in vectors)
+
+    def generate_digital_pattern_source_file(self, pattern_file):
+        pass
 
 if __name__ == '__main__':
     pass
