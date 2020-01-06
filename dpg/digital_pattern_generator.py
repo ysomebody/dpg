d755f4f9dce6460effa46b14db16ba9d5e90b9ce
dennis.ying <dennis.ying@ni.com>
2020-01-06 22:13:51 +0800

    Add empty head and tail

--- dpg/digital_pattern_generator.py
+++ dpg/digital_pattern_generator.py
@@ -13,11 +13,18 @@ class DigitalPatternGenerator:
         with open(self.content_file) as f:
             csv_reader = csv.DictReader(f, delimiter=',')
             content = [row for row in csv_reader]
-        vectors = DigitalPatternGenerator_SPI4W(config).generate(content)
-        return '\n'.join(v.get_statement() for v in vectors)
+        body = self._generate_content_statements(config, content)
+        head = ''
+        tail = ''
+        return '\n'.join([head, body, tail])
 
     def generate_digital_pattern_source_file(self, pattern_file):
         pass
+
+    def _generate_content_statements(self, config, content):
+        dpg = DigitalPatternGenerator_SPI4W(config)
+        vectors = dpg.generate(content)
+        return '\n'.join(v.get_statement() for v in vectors)
 
 if __name__ == '__main__':
     pass
