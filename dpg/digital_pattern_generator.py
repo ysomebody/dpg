8c896f82695e6673810527dea816c7ac66259f27
dennis.ying <dennis.ying@ni.com>
2020-01-07 11:28:00 +0800

    Specify content file in the config file. Write test for DigitalPatternGenerator

--- dpg/digital_pattern_generator.py
+++ dpg/digital_pattern_generator.py
@@ -1,21 +1,32 @@
+import os
 import csv
 import json
 from dpg.spi_4w.spi_4w import DigitalPatternGenerator_SPI4W
 
 class DigitalPatternGenerator:
-    def __init__(self, pattern_config_file, pattern_content_file):
-        self.config_file = pattern_config_file
-        self.content_file = pattern_content_file
+    def __init__(self, config_file=None, config=None, content=None):
+        if config_file:
+            with open(config_file) as f:
+                self.config = json.load(f)
+            config_file_dir = os.path.dirname(os.path.abspath(config_file))
+            content_file = os.path.join(config_file_dir, self.config['content_file'])
+            with open(content_file) as f:
+                csv_reader = csv.DictReader(f, delimiter=',')
+                self.content = [row for row in csv_reader]
+        elif config:
+            self.config = config
+            self.content = content
+        else:
+            raise Exception('Config file or config/content not specified.')
 
-    def generate_digital_pattern_string(self):
-        with open(self.config_file) as f:
-            config = json.load(f)
-        with open(self.content_file) as f:
-            csv_reader = csv.DictReader(f, delimiter=',')
-            content = [row for row in csv_reader]
-        body = self._generate_content_statements(config, content)
-        head = ''
-        tail = ''
+    def generate_digital_pattern_source_string(self):
+        head = self._generate_head(
+            self.config['pattern_name'],
+            self.config['timeset_name'],
+            self.config['pinlist']
+        )
+        body = self._generate_content_statements(self.config, self.content)
+        tail = '}'
         return '\n'.join([head, body, tail])
 
     def generate_digital_pattern_source_file(self, pattern_file):
@@ -24,7 +35,16 @@ class DigitalPatternGenerator:
     def _generate_content_statements(self, config, content):
         dpg = DigitalPatternGenerator_SPI4W(config)
         vectors = dpg.generate(content)
-        return '\n'.join(v.get_statement() for v in vectors)
+        return '\n'.join('\t' + v.get_statement() for v in vectors)
+
+    def _generate_head(self, pattern_name, timeset_name, pinlist):
+        pinlist_str = ','.join(pinlist)
+        return '\n'.join([
+             'file_format_version 1.0;',
+            f'timeset {timeset_name};',
+            f'pattern {pattern_name} ({pinlist_str})',
+             '{'
+        ])
 
 if __name__ == '__main__':
     pass
---
+++ tests/test_digital_pattern_generator.py
@@ -0,0 +1,47 @@
+import unittest
+from dpg.digital_pattern_generator import DigitalPatternGenerator
+
+test_config = {
+    'pattern_name': 'Test_Pattern',
+    'timeset_name': 'Test_TS',
+    'pinlist': ['PinA', 'PinB'],
+    'timeset_between_frames': 2,
+    'content_file': 'test_content.csv'
+}
+
+test_content = [
+    {'Address':'0x101', 'Data':'0xcc'},
+    {'Address':'0x102', 'Data':'0x0'}
+]
+
+class Test_DigitalPatternGenerator_Constructor(unittest.TestCase):
+    def test_init_from_file(self):
+        dpg = DigitalPatternGenerator(config_file = r'tests\test_config.json')
+        self.assertEqual(dpg.config, test_config)
+        self.assertEqual(dpg.content, test_content)
+
+    def test_init_from_arguments(self):
+        dpg = DigitalPatternGenerator(config=test_config, content=test_content)
+        self.assertEqual(dpg.config, test_config)
+        self.assertEqual(dpg.content, test_content)
+
+    def test_init_fail(self):
+        with self.assertRaises(Exception) as ctx:
+            dpg = DigitalPatternGenerator()
+        self.assertIn('not specified', str(ctx.exception))
+
+class Test_DigitalPatternGenerator(unittest.TestCase):
+    def setUp(self):
+        test_content = [
+            {'Address':'0x101', 'Data':'0xcc', 'R/W':'W'},
+            {'Address':'0x102', 'Data':'0x0', 'R/W':'R'}
+        ]
+        self.dpg = DigitalPatternGenerator(config=test_config, content=test_content)
+
+    def test_generate_digital_pattern_source_string(self):
+        srcstr = self.dpg.generate_digital_pattern_source_string()
+        print(srcstr)
+        # TODO: Change to a mock generator
+
+if __name__ == '__main__':
+    unittest.main()
